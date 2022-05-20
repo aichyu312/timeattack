3a90cf890c41a482be84569ee02f732997899468
aichyu312 <aichyu312@gmail.com>
2022-05-20 20:05:10 +0900

    0520 Timeattack 제출

--- 0429/app.py
+++ 0429/app.py
@@ -19,38 +19,45 @@ def index():
 
 @app.route('/post', methods=['POST'])
 def save_post():
-    title_receive = request.form['title_give']
-    password_receive = request.form['password_give']
-    comment_receive = request.form['comment_give']
-    time_receive = request.form['time_give']
+    url_receive = request.form.get('url_give')
+    password_receive = request.form.get('password_give')
+    comment_receive = request.form.get('comment_give')
+    post_count = db.memopost.estimated_document_count({})
+
+    if post_count == 0:
+        max_value = 1
+    else:
+        # max_value = db.test.find_one(sort=[("idx", -1)])['idx'] + 1 이 부분에 질문이 있어서 아래처럼 구현해도 가능합니다.
+        max_value = (list(db.memopost.find({}).sort([("idx", -1)])))[0]['idx'] + 1
 
     doc = {
-        'title': title_receive,
+        'idx': max_value,
+        'url': url_receive,
         'password': password_receive,
-        'time': time_receive,
-        'comment': comment_receive
+        'comment': comment_receive,
+        'reg_date': datetime.now()
     }
 
     db.memopost.insert_one(doc)
 
-    return jsonify({'msg': '저장 완료!'})
+    return {"result": "success"}
 
 
 
 
 @app.route('/post', methods=['GET'])
 def get_post():
-    posts = request.args.get('sample_give')
-    print(posts)
-    return jsonify({"msg": "Get 요청 완료 !"})
+    posts = list(db.memopost.find({}, {'_id': False}).sort("reg_date", -1))
+    for a in posts:
+        a['reg_date'] = a['reg_date'].strftime('%Y.%m.%d %H:%M:%S')
 
-
+    return jsonify({"posts": posts})
 
 
 @app.route('/post', methods=['DELETE'])
 def delete_post():
     idx = request.args.get('idx')
-    db.test.delete_one({'idx': int(idx)})
+    db.memopost.delete_one({'idx': int(idx)})
     return {"result": "success"}
 
 
